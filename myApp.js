e6fc50b1f9c32786be77957cf47853ddb4f53e64
Nina Hahne <ninamariehahne@gmail.com>
2020-07-01 19:34:51 +0200

    uri syntax

--- myApp.js
+++ myApp.js
@@ -15,7 +15,7 @@
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true }); 
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
