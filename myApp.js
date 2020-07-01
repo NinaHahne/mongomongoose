073837c4d955d40d820a387dfbcfb013e44f15eb
Nina Hahne <ninamariehahne@gmail.com>
2020-07-01 19:32:01 +0200

    mongoose setup

--- myApp.js
+++ myApp.js
@@ -14,6 +14,9 @@
 //
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true }); 
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+
 
 
 /** # SCHEMAS and MODELS #
